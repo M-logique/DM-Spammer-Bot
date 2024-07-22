3b7506c6d27c1f24ada17c802017f29f96e2f26a
M-logique <lmy615730@gmail.com>
2024-07-22 17:20:16 +0330

    Fixed JoinSpam file dir and container_name in docker-compose.yml

--- bot/handlers/joinspam.py
+++ bot/handlers/joinspam.py
@@ -11,24 +11,24 @@ class JoinSpam:
             
     @staticmethod
     def append(user_id: str, author: str):
-        with open("joinspam.txt", "a") as file:
+        with open("./data/joinspam.txt", "a") as file:
             file.write("\n%s:%s" % (user_id , author))
         
     @staticmethod
     def remove(user_id: str, author: str):
-        with open("joinspam.txt", "r") as file:
+        with open("./data/joinspam.txt", "r") as file:
             user_ids = [i.strip() for i in file.readlines()]
 
         key = lambda line: not line.startswith("%s:%s" % (user_id, author))
 
         new_ids = [i for i in filter(key, user_ids)]
 
-        with open("joinspam.txt", "w") as file:
+        with open("./data/joinspam.txt", "w") as file:
             file.write("\n".join(new_ids))
     
     @staticmethod
     def user_joinspams(user_id: str):
-        with open("joinspam.txt", "r") as file:
+        with open("./data/joinspam.txt", "r") as file:
             user_ids = [i.strip() for i in file.readlines()]
 
         key = lambda line: line.endswith(user_id)
